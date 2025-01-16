f737be3383762edc87d84cd043ca4001b0b61692
codelifo <madina94495@gmail.com>
2025-01-16 09:54:45 +0600

    user signnIn, signUp, signOut, updateProfile functionalty done.

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../Firebase/firebase.init";
 
 const AuthProviders = ({ children }) => {
-  const name = "ami pare na.";
+  const [user, setUser] = useState(null);
+  const [Loading, setLoading] = useState(true);
+
+  // google Create a password-based account
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // signIn
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Update Profile 
+  const updateUserProfile = (name, photoUrl) =>{
+    return updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photoUrl,
+    })
+  }  
+
+  // signOut 
+  const userLogOut = () =>{
+    setLoading(true);
+    return signOut(auth);
+  }
+
+
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   const authInfo = {
-    name,
+    createUser,
+    updateUserProfile,
+    signIn,
+    Loading,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
